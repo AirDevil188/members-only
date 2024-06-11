1cb4cae1e65d9bb90cb2842af449bae0498e7fa5
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-06-11 13:30:20 +0200

    edited user schema

--- models/user.js
+++ models/user.js
@@ -2,11 +2,9 @@ const mongoose = require("mongose");
 const { Schema } = mongoose;
 
 const UserSchema = new Schema({
-  firstname: { type: String, required: true },
-  lastname: { type: String, required: true },
   username: { type: String, required: true },
   password: { type: String, required: true },
-  member: true,
+  member: false,
 });
 
 module.exports = mongoose.model("User", UserSchema);
