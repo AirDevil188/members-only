f5292cb7a93bc6ac4c8f6129ab4f673611133b7f
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-06-11 13:37:02 +0200

    added members_only_sign_up_post

--- controllers/membersController.js
+++ controllers/membersController.js
@@ -1,8 +1,23 @@
 const mongoose = require("mongoose");
 const asyncHandler = require("express-async-handler");
+const User = require("../models/user");
 
 exports.members_only_sign_up_get = asyncHandler((req, res, next) => {
   res.render("members-sign-up", {
     title: "Members Only - Sign Up",
   });
+});
+
+exports.members_only_sign_up_post = asyncHandler(async (req, res, next) => {
+  try {
+    const user = new User({
+      username: req.body.username,
+      password: req.body.username_password,
+      member: false,
+    });
+    await user.save();
+    res.redirect("/");
+  } catch (err) {
+    return next(err);
+  }
 });
