2af62286519ee324934847e52b7da997df7deb22
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-06-12 15:27:01 +0200

    passed req.user to user object key property, added routes for log in

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 const membersController = require("../controllers/membersController");
+const passport = require("passport");
+const user = require("../models/user");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
@@ -9,13 +11,30 @@ router.get("/", function (req, res, next) {
     req.session.viewCount++;
   } else req.session.viewCount = 1;
   console.log(req.session);
-  res.render("index", { title: "Members Only" });
+  res.render("index", { title: "Members Only", user: req.user });
+  console.log(req.user);
 });
 
 // Sign up GET //
 
 router.get("/sign-up", membersController.members_only_sign_up_get);
 
+// Sign up POST
+
 router.post("/sign-up", membersController.members_only_sign_up_post);
+
+// Log in GET //
+
+router.get("/log-in", membersController.members_only_log_in_get);
+
+// Log in SET //
+
+router.post(
+  "/log-in",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/",
+  })
+);
 
 module.exports = router;
